6b1f9c9df180e6d45f45e8a591b785f224c94076
Mathias Brønd Sørensen <mathias.b.sorensen@gmail.com>
2022-11-01 10:41:38 +0100

    first commit

--- create_splits.py
+++ create_splits.py
@@ -2,10 +2,11 @@ import argparse
 import glob
 import os
 import random
+import tensorflow as tf
 
 import numpy as np
 
-from utils import get_module_logger
+from utils import get_module_logger, get_dataset
 
 
 def split(source, destination):
@@ -18,6 +19,34 @@ def split(source, destination):
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
     # TODO: Implement function
+    
+
+    count = 0
+    # Iterate directory
+    for path in os.listdir(source):
+        # check if current path is a file
+        if os.path.isfile(os.path.join(source, path)):
+            count += 1
+    print('File count:', count)
+
+
+
+    dataset = get_dataset(f"{source}*.tfrecord")
+    dataset = dataset.shuffle(100)
+    #dataset_size = len(list(dataset))
+    dataset_size = 100
+    print(dataset_size)
+    test_size = 3
+    train_size = int(0.8 * dataset_size-test_size)
+    val_size = int(0.2 * dataset_size-test_size)
+    
+
+    test_dataset = dataset.take(test_size)
+    train_dataset = dataset.skip(test_size)
+    val_dataset = dataset.skip(val_size)
+    train_dataset = dataset.take(train_size)
+
+    print('her')
 
 
 if __name__ == "__main__":
